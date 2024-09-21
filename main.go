354874014ce222341dfff3a61f74c154df40f9d2
Navid Lee <1930551858@qq.com>
2024-09-21 20:52:40 +0800

    update version

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, world!",
-			"version": "2.0.0",
+			"host":    os.Hostname(),
+			"version": "3.0.0",
 		})
 	})
 	r.Run()
